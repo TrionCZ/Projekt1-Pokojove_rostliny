9b6d3ae59fdab1d176464ae3c469ce1fbdb51aee
Jan Jugas <janjugas@icloud.com>
2026-04-27 17:20:23 +0200

    zapis informaci o rostline, promenne

--- src/Plant.java
+++ src/Plant.java
@@ -1,2 +1,11 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Plant {
+    //promenne pro nastaveni informaci o rostline
+    private String name;
+    private String notes;
+    private LocalDateTime planted;
+    private LocalDateTime watering;
+    private Duration frequencyOfWatering;
 }
